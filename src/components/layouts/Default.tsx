f58444e62822bc432f8a089bd5af5e52733e5567
sleduardo20 <sl.eduardo20@gmail.com>
2022-11-10 14:57:57 -0300

    ♻ fix styles

--- src/components/layouts/Default.tsx
+++ src/components/layouts/Default.tsx
@@ -1,11 +1,13 @@
 import styled from 'styled-components';
 import { Outlet } from 'react-router-dom';
 import { Header } from '../Header';
+import { Wrapper } from '../Wrapper';
 
 export function DefaultLayout() {
   return (
     <DefaultLayoutContainer>
       <Header />
+
       <Outlet />
     </DefaultLayoutContainer>
   );
