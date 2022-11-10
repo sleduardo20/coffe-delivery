28b851027493d4c7e5d18a149a7f541b588e97e2
sleduardo20 <sl.eduardo20@gmail.com>
2022-11-10 14:53:32 -0300

    🏭 feat - add router confirmOrder

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import { DefaultLayout } from '../components/layouts/Default';
+import { ConfirmOrder } from '../pages/ConfirmOrder';
 import { Home } from '../pages/Home';
 
 export function Router() {
@@ -7,6 +8,7 @@ export function Router() {
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
+        <Route path="/confirmOrder" element={<ConfirmOrder />} />
       </Route>
     </Routes>
   );
