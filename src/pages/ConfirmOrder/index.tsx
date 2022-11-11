82dd5339cc78432b561acdc5701a4b9cec1b0725
sleduardo20 <sl.eduardo20@gmail.com>
2022-11-11 14:15:29 -0300

    🏭 feat - create form confirm order

---
+++ src/pages/ConfirmOrder/index.tsx
@@ -0,0 +1,121 @@
+import {
+  Bank,
+  CreditCard,
+  CurrencyDollar,
+  MapPinLine,
+  Money,
+} from 'phosphor-react';
+import { useState } from 'react';
+import { Input } from '../../components/Input';
+import {
+  CoffesSelectedSection,
+  ConfirmItems,
+  ConfirmOrderContainer,
+  DeliverySection,
+  Field,
+  FinisherOrderSection,
+  FormWrapper,
+  Payment,
+  Title,
+} from './styles';
+
+enum FormPayments {
+  MONEY = 'money',
+  CREDIT_CARD = 'credit-card',
+  DEBIT_CARD = 'debit-card',
+}
+
+export function ConfirmOrder() {
+  const [radioInputValue, setRadioInputValue] = useState<FormPayments>();
+
+  return (
+    <ConfirmOrderContainer>
+      <FormWrapper>
+        <FinisherOrderSection>
+          <h3>Complete seu pedido</h3>
+          <DeliverySection>
+            <Title iconColor="yellowDark">
+              <strong>
+                <MapPinLine weight="regular" size={22} /> Endereço de Entrega
+              </strong>
+              <span>Informe o endereço onde deseja receber seu pedido</span>
+            </Title>
+
+            <Field>
+              <Input placeholder="CEP" width={200} />
+            </Field>
+
+            <Field>
+              <Input placeholder="Rua" />
+            </Field>
+
+            <Field>
+              <Input placeholder="Número" type="number" />
+              <Input placeholder="Complemento" />
+            </Field>
+
+            <Field>
+              <Input placeholder="Bairro" />
+              <Input placeholder="Cidade" />
+              <Input placeholder="UF" width={60} />
+            </Field>
+          </DeliverySection>
+
+          <Payment>
+            <Title iconColor="purple">
+              <strong>
+                <CurrencyDollar weight="regular" size={22} /> Pagamento
+              </strong>
+              <span>
+                O pagamento é feito na entrega. Escolha a forma que deseja pagar
+              </span>
+            </Title>
+
+            <Field>
+              <Input
+                icon={<CreditCard />}
+                name="typePayment"
+                value="credit-card"
+                type="radio"
+                checked={radioInputValue === FormPayments.CREDIT_CARD}
+                label="Cartão de Credito"
+                onChange={(e) =>
+                  setRadioInputValue(e.target.value as FormPayments)
+                }
+              />
+              <Input
+                icon={<Bank />}
+                name="typePayment"
+                value="money"
+                type="radio"
+                label="Dinheiro"
+                checked={radioInputValue === FormPayments.MONEY}
+                onChange={(e) =>
+                  setRadioInputValue(e.target.value as FormPayments)
+                }
+              />
+              <Input
+                icon={<Money />}
+                name="typePayment"
+                value="debit-card"
+                type="radio"
+                label="Cartão de Débito"
+                checked={radioInputValue === FormPayments.DEBIT_CARD}
+                onChange={(e) =>
+                  setRadioInputValue(e.target.value as FormPayments)
+                }
+              />
+            </Field>
+          </Payment>
+        </FinisherOrderSection>
+
+        <CoffesSelectedSection>
+          <h3>Cafés selecionados</h3>
+          <ConfirmItems>
+            <p>items e botao para confirmar</p>
+          </ConfirmItems>
+        </CoffesSelectedSection>
+      </FormWrapper>
+    </ConfirmOrderContainer>
+  );
+}
---
+++ src/pages/ConfirmOrder/styles.ts
@@ -0,0 +1,82 @@
+import styled, { css } from 'styled-components';
+import { Wrapper } from '../../components/Wrapper';
+
+export const ConfirmOrderContainer = styled(Wrapper)``;
+
+export const FormWrapper = styled.form`
+  margin-top: 4rem;
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  gap: 3.2rem;
+
+  h3 {
+    color: ${({ theme }) => theme.subTitle};
+    font-size: 1.8rem;
+    font-family: 'Baloo 2', sans-serif;
+    font-weight: 700;
+    margin-bottom: 1.6rem;
+  }
+`;
+
+export const FinisherOrderSection = styled.section`
+  width: 64rem;
+`;
+
+const DefaultStyles = css`
+  background: ${({ theme }) => theme.card};
+  border-radius: 0.6rem;
+  padding: 4rem;
+`;
+
+export const CoffesSelectedSection = styled.section``;
+
+export const ConfirmItems = styled.div`
+  width: 44.8rem;
+  height: 49.8rem;
+  border-radius: 0.6rem 4.4rem 0.6rem 4.4rem;
+  background: ${({ theme }) => theme.card};
+`;
+
+export const DeliverySection = styled.div`
+  ${DefaultStyles}
+`;
+
+interface TitleProps {
+  iconColor: 'yellowDark' | 'purple';
+}
+
+export const Title = styled.div<TitleProps>`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin-bottom: 3.2rem;
+
+  strong {
+    display: flex;
+    align-items: center;
+    gap: 0.4rem;
+    font-weight: 400;
+  }
+
+  svg {
+    color: ${({ theme, iconColor }) => theme[iconColor]};
+  }
+
+  span {
+    font-size: 1.4rem;
+    margin-left: 2.6rem;
+  }
+`;
+
+export const Field = styled.div`
+  margin-top: 1.6rem;
+  display: flex;
+  align-items: center;
+  gap: 1.2rem;
+`;
+
+export const Payment = styled.section`
+  ${DefaultStyles}
+  margin-top: 1.2rem;
+`;
